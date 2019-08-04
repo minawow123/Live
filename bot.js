9932b7477ec8f5c60b0ce3d1ec46a840e7900ef0
minawow123 <46967902+minawow123@users.noreply.github.com>
2019-08-05 01:50:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : Mina`);
         console.log(`----------------`);
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`Cracking`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
